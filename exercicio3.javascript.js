d872f88db626621217042153c1c5f99c05afe92d
Anefátima Figueiredo <anelenox.vet.domiciliar@gmail.com>
2025-09-01 23:25:05 -0300

    Add files via upload

---
+++ exercicio3.javascript.js
@@ -0,0 +1,12 @@
+javascript
+// 1. Declare uma variável para guardar o nome do usuário (pode mudar)
+let nome = "Maria";
+
+// 2. Declare uma variável para guardar a data de nascimento (não muda)
+const data Nascimento = "10/02/2000";
+
+// 3. Declare uma variável para o saldo bancário (pode mudar)
+let saldo = 1000;
+
+// 4. Declare uma variável para o número de CPF (não muda)
+const cpf = "123.456.789-00";
