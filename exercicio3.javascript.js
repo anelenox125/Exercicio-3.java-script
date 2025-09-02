dd0518722ed98ea3caccdfb9403c0e47da0cbc56
Anefátima Figueiredo <anelenox.vet.domiciliar@gmail.com>
2025-09-01 23:29:50 -0300

    exercicio3.javascript.js

--- exercicio3.javascript.js
+++ exercicio3.javascript.js
@@ -10,3 +10,6 @@ let saldo = 1000;
 
 // 4. Declare uma variável para o número de CPF (não muda)
 const cpf = "123.456.789-00";
+
+Let - é uma variável que se pode abrir e trocar o valor várias vezes. permite sobrescrever o valor, atualizando a memória  RAM com o novo valor.
+Const- é uma variável  que  não se pode trocar o valor depois de colocar algo dentro. Nessa variável a memória RAM não pode apontar para outro valor.   
